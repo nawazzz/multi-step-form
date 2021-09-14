5eab66b7569dee76fc347c753d7243b78f708af1
nawazzz <65383832+nawazzz@users.noreply.github.com>
2021-09-14 22:37:53 +0530

    add modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
 import React from 'react';
+import Modal from 'react-modal';  
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      isModalOpen: false,
     }
+
+  handleModalClose = () => {
+    this.setState({
+      isModalOpen: false
+    })
+  }
+
   }
   render() {
     return (
@@ -24,6 +32,29 @@ class App extends React.Component {
           <div>
             <h3 className="textAlignCenter">3</h3>
             <p>PERSONAL DETAILS</p>
+          </div>
+          <div>
+          <button type="button" onClick={handleOpen}>
+            Open Modal
+          </button>
+          <Modal
+            isOpen={modalIsOpen}
+            onAfterOpen={afterOpenModal}
+            onRequestClose={this.handleModalClose}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+            <button onClick={closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
           </div>
         </div>
       </div>
